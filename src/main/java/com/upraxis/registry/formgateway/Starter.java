28c23d984ca902a6e8290b6663470634dabd032b
Jesse Ballera <jesse_ballera@upraxis.com>
2018-01-17 12:04:17 +0800

    start up

---
+++ src/main/java/com/upraxis/registry/formgateway/Starter.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.upraxis.registry.formgateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author jesse
+ */
+@SpringBootApplication
+public class Starter {
+    
+    public static void main(String[] args) {
+//        SpringApp
+    }
+}
